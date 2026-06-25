c1555e58687eb378de21ee1972d135ba230be4e9
onecocoriki-cpu <onecocoriki@gmail.com>
2026-06-25 11:50:06 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -22,9 +22,10 @@ const db = firebase.firestore();
 //  role: "gudang" | "kepala_gudang" | "manager" | "pembelian"
 // ============================================================
 const USER_ROLES = {
-  "gudang1@perusahaan.com": { role: "gudang", nama: "Budi Santoso", lokasi: "Gudang A" },
-  "gudang2@perusahaan.com": { role: "gudang", nama: "Siti Rahayu", lokasi: "Gudang B" },
-  "kepala.gudang@perusahaan.com": { role: "kepala_gudang", nama: "Ahmad Fauzi", lokasi: "Semua" },
+  "gudang1@perusahaan.com": { role: "gudang", nama: "Fadlur Rohman", lokasi: "Gudang A" },
+  "gudang3@perusahaan.com": { role: "gudang", nama: "Sobikhul Mubarok", lokasi: "Gudang B" },
+  "kepala.gudang1@perusahaan.com": { role: "kepala_gudang", nama: "Amin Bagus Q", lokasi: "Semua" },
+  "kepala.gudang3@perusahaan.com": { role: "kepala_gudang", nama: "Idran Yusuf", lokasi: "semua" },
   "manager@perusahaan.com": { role: "manager", nama: "Dewi Lestari", lokasi: "Semua" },
   "pembelian@perusahaan.com": { role: "pembelian", nama: "Rizky Pratama", lokasi: "Semua" },
 };
